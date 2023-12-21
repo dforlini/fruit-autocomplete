b7cbb6b8cf28ca90f2df4aef1bbba960f18912c5
Tiago Fassoni <tiagofassoni@gmail.com>
2023-12-20 21:16:23 -0300

    Applied Prettier.io on the JS code

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,48 +1,124 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener("DOMContentLoaded", function () {
+  const input = document.querySelector("#fruit");
+  const suggestions = document.querySelector(".suggestions ul");
 
+  const fruit = [
+    "Apple",
+    "Apricot",
+    "Avocado 🥑",
+    "Banana",
+    "Bilberry",
+    "Blackberry",
+    "Blackcurrant",
+    "Blueberry",
+    "Boysenberry",
+    "Currant",
+    "Cherry",
+    "Coconut",
+    "Cranberry",
+    "Cucumber",
+    "Custard apple",
+    "Damson",
+    "Date",
+    "Dragonfruit",
+    "Durian",
+    "Elderberry",
+    "Feijoa",
+    "Fig",
+    "Gooseberry",
+    "Grape",
+    "Raisin",
+    "Grapefruit",
+    "Guava",
+    "Honeyberry",
+    "Huckleberry",
+    "Jabuticaba",
+    "Jackfruit",
+    "Jambul",
+    "Juniper berry",
+    "Kiwifruit",
+    "Kumquat",
+    "Lemon",
+    "Lime",
+    "Loquat",
+    "Longan",
+    "Lychee",
+    "Mango",
+    "Mangosteen",
+    "Marionberry",
+    "Melon",
+    "Cantaloupe",
+    "Honeydew",
+    "Watermelon",
+    "Miracle fruit",
+    "Mulberry",
+    "Nectarine",
+    "Nance",
+    "Olive",
+    "Orange",
+    "Clementine",
+    "Mandarine",
+    "Tangerine",
+    "Papaya",
+    "Passionfruit",
+    "Peach",
+    "Pear",
+    "Persimmon",
+    "Plantain",
+    "Plum",
+    "Pineapple",
+    "Pomegranate",
+    "Pomelo",
+    "Quince",
+    "Raspberry",
+    "Salmonberry",
+    "Rambutan",
+    "Redcurrant",
+    "Salak",
+    "Satsuma",
+    "Soursop",
+    "Star fruit",
+    "Strawberry",
+    "Tamarillo",
+    "Tamarind",
+    "Yuzu",
+  ];
 
-const input = document.querySelector('#fruit');
-const suggestions = document.querySelector('.suggestions ul');
+  function search(str) {
+    console.log(str);
 
-const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+    //let results = []; this part of the starter code not needed, as i used the method filter, which automaticly returns a new array
+    return fruit.filter((fruitName) =>
+      fruitName.toLowerCase().includes(str.toLowerCase())
+    );
+  }
 
-function search(str) {
-	console.log(str);
+  function searchHandler(e) {
+    const inputVal = e.target.value;
+    console.log(inputVal);
 
-	//let results = []; this part of the starter code not needed, as i used the method filter, which automaticly returns a new array
-	return fruit.filter(fruitName => fruitName.toLowerCase().includes(str.toLowerCase()));
+    const results = search(inputVal);
+    console.log(results);
 
+    showSuggestions(results, inputVal);
+  }
 
+  function showSuggestions(results, inputVal) {
+    console.log(inputVal);
+    console.log(results);
 
-}
+    const html = results.map((fruitName) => `<li>${fruitName}</li>`).join("");
+    suggestions.innerHTML = html;
+  }
 
-function searchHandler(e) {
-	const inputVal = e.target.value;
-	console.log(inputVal);
+  function useSuggestion(e) {
+    if (e.target.tagName === "LI") {
+      console.log(e.target.textContent);
+      input.value = e.target.textContent;
+      suggestions.innerHTML = "";
+    }
+  }
 
-	const results = search( inputVal);
-	console.log(results);
-
-	showSuggestions(results, inputVal);
-}
-
-function showSuggestions(results, inputVal) {
-	console.log(inputVal);
-	console.log(results);
-
-	const html = results.map(fruitName => `<li>${fruitName}</li>`).join('');
-	suggestions.innerHTML = html;
-
-}
-
-function useSuggestion(e) {
-	if (e.target.tagName === 'LI') {
-		console.log( e.target.textContent);
-		input.value = e.target.textContent;
-		suggestions.innerHTML = '';
-	}
-}
-
-input.addEventListener('keyup', searchHandler);
-suggestions.addEventListener('click', useSuggestion);
+  input.addEventListener("keyup", searchHandler);
+  suggestions.addEventListener("click", useSuggestion);
 });
